0e4a3678a6ee138a0c89cd8148bd824e22239af9
Elly <blackcoda56@gmail.com>
2023-10-19 09:41:18 +0300

    new database

--- wisdom_academy/settings.py
+++ wisdom_academy/settings.py
@@ -78,14 +78,25 @@ WSGI_APPLICATION = 'wisdom_academy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'wisdom',
+#         'USER': 'root',
+#         'PASSWORD': 'swap2',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'wisdom',
-        'USER': 'root',
-        'PASSWORD': 'swap2',
+        'USER': 'postgres',
+        'PASSWORD': 'swap1',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
 
